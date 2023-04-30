1c93eee2b1e9c83b3a12b9f4380862f7922d3f3d
Mujeeb-Zaman <mujeebzaman007@gmail.com>
2023-04-30 23:07:19 +0500

    Mega Menu Completed, Paused Carousal on Hover, Add to Cart from Home Page, Show details user details on checkout in cart page.

--- main.js
+++ main.js
@@ -27,18 +27,20 @@ let products = [
     }
 ]
 
-for (let i=0; i<carts.length; i++){
-    carts[i].addEventListener('click',()=>{
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
         cartNumbers(products[i]);
         totalCost(products[i]);
+        // setItems(products[i]);
+
     })
 }
 
 function onLoadCartNumbers() {
     let productNumbers = localStorage.getItem('cartNumbers');
-
-    if(productNumbers) {
-        document.querySelector('.cart').textContent = productNumbers;
+    console.log("productNumbers ::", productNumbers)
+    if (productNumbers) {
+        document.querySelector('.cart').innerHTML = productNumbers;
     }
 }
 
@@ -49,10 +51,10 @@ function cartNumbers(product) {
 
     productNumbers = parseInt(productNumbers);
 
-    if(productNumbers) {
+    if (productNumbers) {
         localStorage.setItem('cartNumbers', productNumbers + 1);
         document.querySelector('.cart').textContent = productNumbers + 1;
-    } else{
+    } else {
         localStorage.setItem('cartNumbers', 1);
         document.querySelector('.cart').textContent = 1;
     }
@@ -60,12 +62,12 @@ function cartNumbers(product) {
     setItems(product);
 }
 
-function setItems(product){
+function setItems(product) {
     let cartItems = localStorage.getItem('productsInCart');
     cartItems = JSON.parse(cartItems);
 
-    if(cartItems != null) {
-        if(cartItems[product.tag] == undefined) {
+    if (cartItems != null) {
+        if (cartItems[product.tag] == undefined) {
             cartItems = {
                 ...cartItems,
                 [product.tag]: product
@@ -78,18 +80,75 @@ function setItems(product){
             [product.tag]: product
         }
     }
-        localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
 function totalCost(product) {
     let cartCost = localStorage.getItem('totalCost');
 
-    if(cartCost != null){
+    if (cartCost != null) {
         cartCost = parseInt(cartCost);
         localStorage.setItem("totalCost", cartCost + product.price);
-    }else{
+    } else {
         localStorage.setItem("totalCost", product.price);
     }
 }
 
+
+
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem("totalcost");
+
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            console.log(item);
+            productContainer.innerHTML += `
+            <div class="test">
+            <div>
+                ${item.name}
+           </div>
+            <div>$          ${item.price}.00</div>
+            <div >
+                <ion-icon class="decrease" name="arrow-dropleft-circle"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon class="increase" name="arrow-dropright-circle"></ion-icon>
+            </div>
+            <div >
+                $${item.inCart * item.price},00
+            </div>
+            </div>
+            `;
+        });
+
+        productContainer.innnerHTML += `
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">
+                    Basket Total
+                </h4>
+                <h4 class="basketTotal">
+                    $${cartCost},00 
+                </h4>
+        `;
+    }
+}
+
+
+
+
+window.onload = function () {
+    console.log("hellow")
+    displayCart();
+};
+
+
+const signUpHandler = () => {
+    const firstName = document.getElementById("#firstName").value;
+    console.log("firstName ::", firstName)
+}
+
 onLoadCartNumbers();
+// displayCart()
